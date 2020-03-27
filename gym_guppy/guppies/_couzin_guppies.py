Decreased ZOA for CouzinGuppies, fixed bug in computation of zone indices, added `couzin_zones` property, replaced `adaptive_couzin_zones()` by `couzin_zones` in AdaptiveCouzinGuppy`

gym_guppy/guppies/_couzin_guppies.py
@@ -12,7 +12,7 @@ from gym_guppy.tools.math import rotation, row_norm
 
 _ZOR = 0.005  # zone of repulsion
 _ZOO = 0.09  # zone of orientation
-_ZOA = 1.0  # zone of attraction
+_ZOA = 0.4  # zone of attraction
 _FOP = np.deg2rad(320)  # field of perception
 _ZOI = _ZOR + _ZOO + _ZOA
 
@@ -47,7 +47,7 @@ def _compute_zone_indices(dist, phi, zor=_ZOR, zoo=_ZOO, zoa=_ZOA, field_of_perc
 
     i_r = np.logical_and(0.0 < dist, dist <= zor)
     i_o = np.logical_and(np.logical_and(zor < dist, dist <= zor + zoo), i_fop)
-    i_a = np.logical_and(np.logical_and(zor + zoo < dist, dist <= zoa), i_fop)
+    i_a = np.logical_and(np.logical_and(zor + zoo < dist, dist <= zor + zoo + zoa), i_fop)
     # print(f"i_fop: {i_fop} n_r: {sum(i_r)}; n_o: {sum(i_o)}; n_a: {sum(i_a)} ", end='')
 
     return i_r, i_o, i_a, i_fop
@@ -184,6 +184,10 @@ class BaseCouzinGuppy(Guppy, abc.ABC):
     def zoi(self):
         return _ZOI
 
+    @property
+    def couzin_zones(self):
+        return _ZOR, _ZOO, _ZOA
+
 
 class ClassicCouzinGuppy(BaseCouzinGuppy, ConstantVelocityAgent):
     def __init__(self, **kwargs):
@@ -255,8 +259,9 @@ class AdaptiveCouzinGuppy(BoostCouzinGuppy):
 
         self._zone_cache = zor, zoo, zoa
 
-    def adaptive_couzin_zones(self):
-        return self._zone_cache
+    @property
+    def couzin_zones(self):
+        return self.zor, self.zoo, self.zoa
 
     def compute_next_action(self, state: np.ndarray, kd_tree: cKDTree = None):
         k = min(self._k_neighbors, len(state))
@@ -311,19 +316,19 @@ class AdaptiveCouzinGuppy(BoostCouzinGuppy):
 
     @property
     def zor(self):
-        return self._zone_cache[0]
+        return self._zone_cache[0].squeeze()
 
     @property
     def zoo(self):
-        return self._zone_cache[1]
+        return self._zone_cache[1].squeeze()
 
     @property
     def zoa(self):
-        return self._zone_cache[2]
+        return self._zone_cache[2].squeeze()
 
     @property
     def zoi(self):
-        return sum(self._zone_cache)
+        return np.sum(self._zone_cache, axis=-1).squeeze()
 
 
 class BiasedAdaptiveCouzinGuppy(AdaptiveCouzinGuppy):

gym_guppy/guppies/_randomized_guppies.py
@@ -35,12 +35,12 @@ class RandomizedCouzinGuppy(BiasedAdaptiveCouzinGuppy):
         self._zoo_factor = 1.0 + np.abs(rng.randn()) * zoo_factor_noise
 
     # overrides parent method
-    def adaptive_couzin_zones(self):
+    def _update_zones(self):
         zor = self._zone_radius * self._adaptive_zone_factors
         zoo = (self._zone_radius - zor) * self._adaptive_zone_factors * self._zoo_factor
         zoa = self._zone_radius - zoo - zor
-        # print('couzin_zones', zor, zoo, zoa)
-        return zor, zoo, zoa
+
+        self._zone_cache = zor, zoo, zoa
 
     @property
     def dr_parameter_list(self):

gym_guppy/wrappers/evaluation_wrapper.py
@@ -69,7 +69,7 @@ class EvaluationWrapper(gym.Wrapper):
         self.group['distance_to_fish'][self.step_counter] = distance_to_fish(current_state)
 
         guppy = next(self.env.guppies)
-        self.group['zones_size'][self.step_counter] = np.array(guppy.adaptive_couzin_zones(),
+        self.group['zones_size'][self.step_counter] = np.array(guppy.couzin_zones,
                                                                dtype=np.float32).flatten()
         self.step_counter += 1
 
@@ -102,7 +102,7 @@ class OmniscienceWrapper(gym.Wrapper):
         dr_parameters = np.array \
             ([g.dr_parameter_list for g in self.env.guppies if isinstance(g, RandomizedCouzinGuppy)]).flatten()
         zones = np.array \
-            ([g.adaptive_couzin_zones() for g in self.env.guppies if isinstance(g, AdaptiveCouzinGuppy)]).flatten()
+            ([g.couzin_zones for g in self.env.guppies if isinstance(g, AdaptiveCouzinGuppy)]).flatten()
         time_left = np.array([(self.max_time_steps - self.t) / self.max_time_steps]).flatten()
         info = np.concatenate((dr_parameters, zones, time_left), axis=0)
         print(info.shape)

gym_guppy/wrappers/observation_wrapper.py
@@ -185,7 +185,7 @@ class TrackAdaptiveZones(gym.ObservationWrapper):
         return observation
 
     def zones(self):
-        return np.array([g.adaptive_couzin_zones() for g in self.env.guppies
+        return np.array([g.couzin_zones for g in self.env.guppies
                          if isinstance(g, AdaptiveCouzinGuppy)]).flatten()
 
 